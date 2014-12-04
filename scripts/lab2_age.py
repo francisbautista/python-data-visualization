6adcfd7740ef85cf2d0450475c04292733b534a9
Francis Bautista <francis.bautista07@gmail.com>
2014-12-04 14:53:40 +0800

    Included age sum

--- scripts/lab2_age.py
+++ scripts/lab2_age.py
@@ -12,34 +12,34 @@ population,lifeExpectancy,incomePerCapita,expenditurePerCapita=loadtxt(fn,unpack
 
 def main():
     region_list = unique(region)
-    population_dict = {}
+    age_dict = {}
     for i in range(len(region_list)):
-        population_dict[region_list[i]] = popCounter(region_list[i])
-    sort_dict = collections.OrderedDict(sorted(population_dict.items()))
+        age_dict[region_list[i]] = ageCounter(region_list[i])
+    sort_dict = collections.OrderedDict(sorted(age_dict.items()))
     for key in sort_dict:
         print "%s: %s" % (key, sort_dict[key])
     graph(sort_dict)
     sys.exit(1)
 
 # Iterates through each row with the same Region
-# and adds population to sum and returns sum value
-def popCounter(region_val):
+# and adds age to sum and returns sum value
+def ageCounter(region_val):
     sum = 0
     reg_index = where(region==region_val)
     for i in range(0,len(reg_index[0])):
-        sum = sum + population[reg_index[0][i]]
+        sum = sum + lifeExpectancy[reg_index[0][i]]
     return sum
 
 # Set x and y labels and values for barplot
 # and then graph values and save in fig folder
 def graph(sort_dict):
-    plt.bar(range(len(sort_dict)), sort_dict.values(), width=1)
-    plt.xticks(range(len(sort_dict)), sort_dict.keys(), rotation=45, size='small')
-    plt.ylim((0,15000000))
-    plt.xlabel("Population")
+    plt.yticks(range(len(sort_dict)), sort_dict.keys(), rotation=45, size='small')
+    plt.barh(range(len(sort_dict)), sort_dict.values())
+    plt.xlim((0,500))
+    plt.xlabel("Life Expectancy")
     plt.ylabel("Regions")
-    plt.title("Regional Populations of the Philippines")
-    plt.savefig("../fig/Regional Population.png")
+    plt.title("Regional Life Expectancy of the Philippines")
+    plt.savefig("../fig/Life Expectancy.png")
     plt.show()
 
 if __name__ == '__main__':
